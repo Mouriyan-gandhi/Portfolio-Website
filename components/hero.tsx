3b07f2a58d116c1a0b14a9895ca44b2386435fcc
Mouriyan-gandhi <gandhimouriyan1234@gmail.com>
2026-09-13 13:49:19 +0530

    Update hero section image

--- components/hero.tsx
+++ components/hero.tsx
@@ -143,7 +143,7 @@ export default function Hero() {
               <div className="absolute inset-0 flex items-center justify-center">
                 <div className="w-[90%] h-[90%] rounded-full overflow-hidden border-4 border-white/30 shadow-xl">
                   <Image
-                    src="/images/nishtha-deep.png"
+                    src="/images/herosection-img.jpeg"
                     alt="Nishtha Deep - UI/UX Designer"
                     width={500}
                     height={500}
